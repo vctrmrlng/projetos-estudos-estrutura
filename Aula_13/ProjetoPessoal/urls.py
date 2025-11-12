c2d7e238d5ff032c3fa4b5c952ad525cfbc9361c
Victor M. <vmerling02@gmail.com>
2025-11-12 11:35:47 -0300

    Aula 2025-11-12

--- Aula_13/ProjetoPessoal/urls.py
+++ Aula_13/ProjetoPessoal/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+
+def tela(request):
+    return HttpResponse("Olá Mundo!")
+
+def home(request):
+    return HttpResponse("Seja Bem Vindo! 😍")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tela/', tela),
+    path('', home),
 ]
